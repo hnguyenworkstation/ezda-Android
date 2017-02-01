2323c3093c2261e98c8f5cd101a5370cc35aab49
Hung Nguyen <hnguyenworkstation@gmail.com>
2017-02-01 13:49:15 -0500

    forked from Tinder View

---
+++ Ezda/app/src/main/java/us/hungnguyen/ezda/Adapters/CardStackAdapter.java
@@ -0,0 +1,32 @@
+package us.hungnguyen.ezda.Adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import us.hungnguyen.ezda.R;
+
+
+/**
+ * Created by Aradh Pillaion 07/10/15.
+ */
+
+public class CardStackAdapter extends ArrayAdapter<String> {
+
+
+    public CardStackAdapter(Context context, int resource) {
+
+        super(context, 0);
+    }
+
+    @Override
+    public View getView(int position, final View contentView, ViewGroup parent) {
+        //supply the layout for your card
+        TextView v = (TextView) (contentView.findViewById(R.id.helloText));
+        v.setText(getItem(position));
+        return contentView;
+    }
+
+}
